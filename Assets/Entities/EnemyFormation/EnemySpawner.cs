54048e72fe8b7464cf908b1e05b677ee1412b9f3
CJ <vssrcj@gmail.com>
2017-08-08 23:37:22 +0200

    init enemy formation

---
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+        foreach (Transform child in transform)
+        {
+            var enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
